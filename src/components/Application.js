a9763e457acf3171bd1493e855e979497fff2a67
Alan Marx <alanmarx@Valerias-MacBook-Pro.local>
2020-04-16 10:48:07 -0700

    all state in application.js is created and updated together

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,10 +56,18 @@ const appointments = [
 ];
 
 
+
 export default function Application(props) {
 
-  const [selectedDay, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day});
+  const setDays = days => setState(prev => ({ ...state, days}));
+  
 
   useEffect(() => {
     axios.get('/api/days')
@@ -82,8 +90,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          day={selectedDay}
+          days={state.days}
+          day={state.day}
           setDay={setDay} 
         />
       </nav>
